c5257a452f892c4e6412742bd5317e8270b014a3
N0VN746 <jeffreymonay8@gmail.com>
2026-07-09 13:03:12 +0800

    Revert to First version

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World! I am N0VN746, the creater of this cli helloworld" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
